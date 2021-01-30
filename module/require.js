766b28fe57d3cdbed6646c105a0df8fac07955a3
Griffin <garam.dev9@gmail.com>
2021-01-30 20:53:46 +0900

    Update: require Comment

--- module/require.js
+++ module/require.js
@@ -1,10 +1,14 @@
 console.log('require 가 가장 위에 오지 않아도 됩니다.');
 
+// module.exports 가 최하단에 위치할 필요는 없다.
 module.exports = '저를 찾아보세요.';
 
+// require 가 반드시 파일 최상단에 위치할 필요는 없다.
 require('./var');
 
 console.log('require.cache 입니다.');
+// 한 번 require 한 파일은 require.cache 에 저장되므로 다음번 require 할 때는 새로 불러오지않고 cache 에 있는 것을 사용
+// 새로 require 하기를 원한다면 require.cache 속성을 제거 (권장 X)
 console.log(require.cache);
 /**
  * [Object: null prototype] {
