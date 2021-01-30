2b83c754f4b7a93a46824e26a252c6d4719881dc
Griffin <garam.dev9@gmail.com>
2021-01-30 20:42:40 +0900

    Add: exports 객체 사용

--- module/var.js
+++ module/var.js
@@ -1,7 +1,13 @@
-const odd = '홀수입니다.';
-const even = '짝수입니다.';
+// exports 객체 사용: 객체만 사용가능 (함수 X)
+// 한 모듈에 exports 객체와 module.exports 를 동시에 사용하지 않는 것이 좋다.
+// exports ---참조---> module.exports ---참조---> {}
+exports.odd = '홀수입니다.';
+exports.even = '짝수입니다.';
 
-module.exports = {
-    odd,
-    even
-}
+// const odd = '홀수입니다.';
+// const even = '짝수입니다.';
+
+// module.exports = {
+//     odd,
+//     even
+// }
