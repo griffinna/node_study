55e5447aa1b0a4942a8f1de65a808804cabb7e6f
Griffin <garam.dev9@gmail.com>
2021-01-31 13:12:46 +0900

    Update: os module comment

--- inner_module/os.js
+++ inner_module/os.js
@@ -1,3 +1,4 @@
+// os 모듈은 주로 컴퓨터 내부 자원에 빈번하게 접근하는 경우 사용
 const os = require('os');
 
 console.log('운영체제 정보 --------------');
@@ -19,3 +20,6 @@ console.log('os.cpus().length: ', os.cpus().length);
 console.log('메모리 정보 ---------------------');
 console.log('os.freemem(): ', os.freemem());
 console.log('os.totalmem(): ', os.totalmem());
+
+// os.constants: 각종 에러와 신호에 대한 정보가 담겨있음
+// 에러가 발생했을 때, EADDRINUSE, ECONNRESET 같은 에러 코드를 함께 보여줌
